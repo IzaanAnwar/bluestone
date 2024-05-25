036e50aae7056d762ea780738c1906121f23105b
IzaanAnwar <mdizaan67@gmail.com>
2024-05-25 20:43:31 +0530

    landing

---
+++ src/app/topbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, } from "@nextui-org/navbar";
+import { Button} from "@nextui-org/button";
+import { Link} from "@nextui-org/link";
+import { ServerIcon } from "lucide-react";
+
+
+export  function Topbar() {
+  return (
+    <Navbar shouldHideOnScroll>
+      <NavbarBrand className="gap-2">
+      <ServerIcon className="h-6 w-6" />
+        <p className="font-bold text-inherit">Bluestine Tech Services</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#about1">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#services" aria-current="page">
+            Services
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#contact">
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
